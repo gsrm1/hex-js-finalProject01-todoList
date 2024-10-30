445f616702900704b8876c17a8e68ad7c0e34f2f
gsrm1 <gsrm1@hotmail.com>
2024-10-30 22:31:43 +0800

    10/30 up

--- all.js
+++ all.js
@@ -5,19 +5,19 @@ let listData = [
     checked: '',
   },
   {
-    text: '完成TodoList作業',
+    text: '繳交TodoList作業',
+    checked: 'checked',
+  },
+  {
+    text: '找到一份前端工程師工作',
     checked: '',
   },
 ];
-
 //渲染功能
 const list = document.querySelector('#list');
-const workingNum = document.querySelector('.list_footer p');
-
-function renderList() {
+function renderList(array) {
   let str = '';
-  let listNum = 0;
-  listData.forEach(function (item, index) {
+  array.forEach(function (item, index) {
     str += ` <li data-listNumIndex="${index}"><label class="checkbox">
                 <input type="checkbox" name="checkbox" ${item.checked}/>
                 <span>${item.text}</span>
@@ -26,14 +26,13 @@ function renderList() {
             </li> `;
   });
   list.innerHTML = str;
-
-  listNum = listData.filter(function (item) {
+  const workingNum = document.querySelector('.list_footer p');
+  let workingItems = listData.filter(function (item) {
     return item.checked == '';
   });
-  workingNum.textContent = `${listNum.length}個待完成項目`;
+  workingNum.textContent = `${workingItems.length}個待完成項目`;
 }
 renderList(listData);
-
 //新增清單功能：提醒未輸入、將輸入內容帶入初始陣列
 const todoInput = document.querySelector('#todoInput');
 function saveTodo() {
@@ -46,21 +45,19 @@ function saveTodo() {
   };
   newTodo.text = todoInput.value;
   listData.push(newTodo);
-  renderList();
+  todoInput.value = '';
+  updateList();
 }
 //新增清單功能：送出方式（按鈕與鍵盤Enter）
 const addBtn = document.querySelector('#btn_add');
 addBtn.addEventListener('click', function (e) {
   saveTodo();
-  todoInput.value = '';
 });
 todoInput.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
     saveTodo();
-    todoInput.value = '';
   }
 });
-
 //刪除 & 標示待完成
 list.addEventListener('click', function (e) {
   if (e.target.getAttribute('class') === 'delete') {
@@ -70,7 +67,7 @@ list.addEventListener('click', function (e) {
     if (deleteConfirm) {
       listData.splice(deleteByIndex, 1);
     }
-    renderList(listData);
+    updateList();
   } else {
     let listNumIndex = e.target.closest('li').getAttribute('data-listNumIndex');
     listData.forEach(function (item, index) {
@@ -81,44 +78,42 @@ list.addEventListener('click', function (e) {
         } else {
           listData[index].checked = 'checked';
         }
-        // classifyTodo();
+        updateList();
       }
     });
   }
 });
-
+//更新功能
+let classifiedData = [];
+function updateList() {
+  if (toggleStatus == '全部') {
+    classifiedData = listData;
+  } else if (toggleStatus == '待完成') {
+    classifiedData = listData.filter((item) => item.checked == '');
+  } else if (toggleStatus == '已完成') {
+    classifiedData = listData.filter((item) => item.checked == 'checked');
+  }
+  renderList(classifiedData);
+}
 //tab切換：切換tab樣式、紀錄選取tab頁簽的狀態
 const tabUl = document.querySelector('#tab');
-let toggleStatus = 'all';
-
-tabUl.addEventListener('click', changeTab);
-function changeTab(e) {
+let toggleStatus = '全部';
+tabUl.addEventListener('click', function (e) {
+  toggleStatus = e.target.innerText;
   let tabLi = document.querySelectorAll('#tab li');
-  tabLi.forEach(function (item) {
-    item.classList.remove('active');
+  tabLi.forEach(function (i) {
+    i.classList.remove('active');
   });
   e.target.classList.add('active');
-  toggleStatus = e.target.getAttribute('data-tab');
-  classifyTodo(); //使用分類完成狀態功能
-}
-
-//分類完成狀態功能
-function classifyTodo() {
-  let classifiedListData = [];
-  if (toggleStatus == 'all') {
-    classifiedListData = listData;
-  } else if (toggleStatus == 'working') {
-    classifiedListData = listData.filter(function (item) {
-      return item.checked == '';
-    });
-  } else if (toggleStatus == 'done') {
-    classifiedListData = listData.filter(function (item) {
-      return item.checked == 'checked';
-    });
-  }
-  renderList(classifiedListData)
-  console.log(classifiedListData);
-  
-}
-
+  updateList(); //使用分類完成狀態功能
+});
+//刪除已完成功能
 const clearDoneList = document.querySelector('.list_footer a');
+clearDoneList.addEventListener('click', function (e) {
+  e.preventDefault();
+  const deleteDoneConfirm = confirm('確定清除已完成項目嗎？');
+    if (deleteDoneConfirm) {
+      listData = listData.filter((item) => item.checked == '');
+    }
+  updateList();
+});
